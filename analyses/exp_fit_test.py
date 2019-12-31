42caf840ce299bdda0200a9de31ef69bddc27498
Luke Campagnola <luke.campagnola@gmail.com>
2019-12-31 13:37:41 -0800

    test working to compare several fitting methods

--- analyses/exp_fit_test.py
+++ analyses/exp_fit_test.py
@@ -1,106 +1,87 @@
 """
 For exploring and comparing curve fitting methods.
 """
-
-
+import time
 import numpy as np
 import scipy.optimize, scipy.ndimage
 import pyqtgraph as pg
 import pyqtgraph.multiprocess as mp
 
-sample_rate = 50000
-dt = 1.0 / sample_rate
-
-
-
-def make_noise(t):
-    kernel_t = np.arange(0, 0.1, dt)
-    kernel = 0.01 * np.exp(-kernel_t / 50e-3)
-
-    # noise = 0.02 * np.random.normal(size=len(t))
-    # noise = scipy.ndimage.gaussian_filter(noise, 4)
-    noise = 0.002 * np.random.normal(size=len(t) + len(kernel_t))
-    noise = np.convolve(noise, kernel, 'valid')
-
-    return noise[:len(t)]
-
-
-
-def exp_fn(params, t):
-    (yoffset, amp, tau) = params
-    return yoffset + amp * np.exp(-t / tau)
-
-
-def exp_err_fn(params, t, y):
-    residual = y - exp_fn(params, t)
-    return np.linalg.norm(residual)
-
-
-def exp_jac_fn(params, t, y):
-    x0, x1, x2 = params
-    N = len(y)
-
-    norm = np.sqrt(((x0 + x1 * np.exp(-t/x2) - y) ** 2).sum())
-    exp_t_tau = np.exp(-t / x2)
-    dx0 = (N * x0 + (x1 * exp_t_tau).sum() - y.sum()) / norm
-    dx1 = ((x0 + x1 * exp_t_tau - y) * exp_t_tau).sum() / norm
-    dx2 = (x1 * (x0 + x1* exp_t_tau - y) * exp_t_tau * t / x2**2).sum() / norm
-
-    return np.array([dx0, dx1, dx2])
-
 
 class ExpFitMethod:
+    params = ['yoffset', 'amp', 'tau']
     dtype = [
         ('fit', object),
         ('yoffset', float),
         ('amp', float),
         ('tau', float),
+        ('init_yoffset', float),
+        ('init_amp', float),
+        ('init_tau', float),
         ('err', float),
         ('nfev', int),
         ('success', bool),
+        ('fit_time', float),
     ]
 
-    params = ['yoffset', 'amp', 'tau']
-
-    def __init__(self, name, use_jac=True):
+    def __init__(self, name, use_jac=True, method=None):
         self.name = name
+        self.method = method
         self.use_jac = use_jac
     
     def fit(self, y, t):
+        start = time.time()
         yoff = y[-1]
         amp = y[0] - yoff
         tau = t[-1] - t[0]
         init = (yoff, amp, tau)
         args = (t, y)
-        jac_fn = exp_jac_fn if self.use_jac else None
-        fit = scipy.optimize.minimize(fun=exp_err_fn, x0=init, args=args, jac=jac_fn)
+        jac_fn = self.exp_jac_fn if self.use_jac else None
+        fit = scipy.optimize.minimize(fun=self.exp_err_fn, x0=init, args=args, jac=jac_fn, method=self.method)
         return {
             'fit': fit,
             'yoffset': fit.x[0],
             'amp': fit.x[1],
             'tau': fit.x[2],
+            'init_yoffset': init[0],
+            'init_amp': init[1],
+            'init_tau': init[2],
             'err': fit.fun,
             'nfev': fit.nfev,
             'success': fit.success,
+            'fit_time': time.time() - start,
         }
 
     def eval(self, result, t):
         x = (result['yoffset'], result['amp'], result['tau'])
-        return exp_fn(x, t)
+        return self.exp_fn(x, t)
 
-    
+    @staticmethod
+    def exp_fn(params, t):
+        (yoffset, amp, tau) = params
+        return yoffset + amp * np.exp(-t / tau)
+
+    @staticmethod
+    def exp_err_fn(params, t, y):
+        residual = y - ExpFitMethod.exp_fn(params, t)
+        return np.linalg.norm(residual)
+
+    @staticmethod
+    def exp_jac_fn(params, t, y):
+        x0, x1, x2 = params
+        N = len(y)
+
+        norm = np.sqrt(((x0 + x1 * np.exp(-t/x2) - y) ** 2).sum())
+        exp_t_tau = np.exp(-t / x2)
+        dx0 = (N * x0 + (x1 * exp_t_tau).sum() - y.sum()) / norm
+        dx1 = ((x0 + x1 * exp_t_tau - y) * exp_t_tau).sum() / norm
+        dx2 = (x1 * (x0 + x1* exp_t_tau - y) * exp_t_tau * t / x2**2).sum() / norm
+
+        return np.array([dx0, dx1, dx2])
 
 
-
-
-
-if __name__ == '__main__':
-    pg.mkQApp()
-    pg.dbg()
-
-    t = np.arange(0, 0.4, dt)
-    N = 100
-
+class ExpGenerator:
+    params = ['yoffset', 'amp', 'tau']
     dtype = [
         ('x', object),
         ('y', object),
@@ -109,98 +90,131 @@ if __name__ == '__main__':
         ('yoffset', float),
         ('amp', float),
         ('tau', float),
-        ('yoffset_err', float),
-        ('amp_err', float),
-        ('tau_err', float),
     ]
+
+    def __init__(self, duration=0.4, sample_rate=50000):
+        self.sample_rate = sample_rate
+        self.dt = 1.0 / sample_rate
+        self.t = np.arange(0, duration, self.dt)
+        
+    def make_example(self):
+        yoffset = np.random.uniform(-80e-3, -60e-3)
+        amp = np.random.uniform(-100e-3, 100e-3)
+        tau = np.random.uniform(5e-3, 500e-3)
+
+        x = yoffset, amp, tau
+        true_y = ExpFitMethod.exp_fn(x, self.t)
+        y = true_y + self.make_noise(self.t)
+
+        return {
+            'x': x, 
+            'y': y,
+            't': self.t,
+            'true_y': true_y,
+            'yoffset': x[0],
+            'amp': x[1],
+            'tau': x[2],
+        }
+
+    def make_noise(self, t):
+        kernel_t = np.arange(0, 0.1, self.dt)
+        kernel = 0.01 * np.exp(-kernel_t / 50e-3)
+
+        # noise = 0.02 * np.random.normal(size=len(t))
+        # noise = scipy.ndimage.gaussian_filter(noise, 4)
+        noise = 0.002 * np.random.normal(size=len(t) + len(kernel_t))
+        noise = np.convolve(noise, kernel, 'valid')
+
+        return noise[:len(t)]
+
+
+
+if __name__ == '__main__':
+    pg.mkQApp()
+    pg.dbg()
+
+    sample_rate = 50000
+    N = 1000
+
+    generator = ExpGenerator(duration=0.4, sample_rate=sample_rate)
     
     methods = [
         ExpFitMethod(name='minimize_wo_jac', use_jac=False),
-        ExpFitMethod(name='minimize_w_jac', use_jac = True),
+        ExpFitMethod(name='minimize_w_jac_slsqp', use_jac=True, method='SLSQP'),
+        ExpFitMethod(name='minimize_w_jac_cobyla', use_jac=True, method='COBYLA'),
+        ExpFitMethod(name='minimize_w_jac_bfgs', use_jac=True, method='BFGS'),
+        # ExpFitMethod(name='minimize_w_jac_dogleg', use_jac=True, method='dogleg'),   # !!!
+        ExpFitMethod(name='minimize_w_jac_l_bfgs_b', use_jac=True, method='L-BFGS-B'),
     ]
 
+    dtype = generator.dtype.copy()
 
     for method in methods:
         pfx = method.name + '_'
         for field in method.dtype:
             dtype.append((pfx + field[0], field[1]))
         dtype.extend([
+            (pfx+'fit_y', object),
             (pfx+'true_err', float),
-        ]
+        ])
         for par_name in method.params:
             dtype.append((pfx + par_name + '_err', float))
-
 
     examples = np.empty(N, dtype=dtype)
 
     with pg.ProgressDialog("making some noise..", maximum=N) as dlg:
         for i in range(N):
-            ex = examples[i]
-
-            yoffset = np.random.uniform(-80e-3, -60e-3)
-            amp = np.random.uniform(-100e-3, 100e-3)
-            tau = np.random.uniform(5e-3, 500e-3)
-
-            x = yoffset, amp, tau
-            true_y = exp_fn(x, t)
-            y = true_y + make_noise(t)
-
-            ex['x'] = x
-            ex['y'] = y
-            ex['t'] = t
-            ex['true_y'] = true_y
-            ex['yoffset'] = x[0]
-            ex['amp'] = x[1]
-            ex['tau'] = x[2]
-
+            ex = generator.make_example()
+            for k,v in ex.items():
+                examples[i][k] = v
             dlg += 1
 
-
-    results = []
-    # with mp.Parallelize(range(N), results=results, progressDialog="fitting, don't you think?", workers=1) as tasker:
-        # for i in tasker:
     with pg.ProgressDialog("fitting, don't you think?", maximum=N) as dlg:
         for i in range(N):
             ex = examples[i]
             y = ex['y']
             t = ex['t']
-            result = {}
+            x = ex['x']
+            true_y = ex['true_y']
             for method in methods:
-                result[method.name] = method.fit(t, y)
-            results.append(result)
+                pfx = method.name + '_'
+                result = method.fit(y, t)
+                for k,v in result.items():
+                    ex[pfx+k] = v
+                fit_y = method.eval(result, t)
+                ex[pfx+'fit_y'] = fit_y
+                ex[pfx+'true_err'] = np.linalg.norm(true_y - fit_y)
+                for pname in method.params:
+                    ex[pfx+pname+'_err'] = ex[pname] - result[pname]
             dlg += 1
 
 
-    with pg.ProgressDialog("quantifying life mistakes..", maximum=N) as dlg:
-        for i,result in enumerate(results):
-            for method_name, fit in result:
-                ex = examples[i]
+    # with pg.ProgressDialog("quantifying life mistakes..", maximum=N) as dlg:
+    #     for i,result in enumerate(results):
+    #         for method_name, fit in result:
+    #             ex = examples[i]
 
-                x = ex['x']
-                true_y = ex['true_y']
-                fit_y = exp_fn(fit_x, t)
-                true_err = np.linalg.norm(true_y - fit_y)
 
-                pfx = method.name + '_'
-                for i,param in enumerate(method.params):
-                    ex[pfx+param] = 
+    #             for i,param in enumerate(method.params):
+    #                 ex[pfx+param] = 
 
-                ex['fit_yoffset'] = fit_x[0]
-                ex['fit_amp'] = fit_x[1]
-                ex['fit_tau'] = fit_x[2]
-                ex['fit'] = fit
-                ex['fit_y'] = fit_y
-                ex['fit_err'] = fit_err
-                ex['true_err'] = true_err
-                ex['yoffset_err'] = x[0] - fit_x[0]
-                ex['amp_err'] = x[1] - fit_x[1]
-                ex['tau_err'] = x[2] - fit_x[2]
-                ex['fit_success'] = fit_success
-                ex['fit_nfev'] = fit_nfev
+    #             ex['fit_yoffset'] = fit_x[0]
+    #             ex['fit_amp'] = fit_x[1]
+    #             ex['fit_tau'] = fit_x[2]
+    #             ex['fit'] = fit
+    #             ex['fit_y'] = fit_y
+    #             ex['fit_err'] = fit_err
+    #             ex['true_err'] = true_err
+    #             ex['yoffset_err'] = x[0] - fit_x[0]
+    #             ex['amp_err'] = x[1] - fit_x[1]
+    #             ex['tau_err'] = x[2] - fit_x[2]
+    #             ex['fit_success'] = fit_success
+    #             ex['fit_nfev'] = fit_nfev
 
-                dlg += 1
+    #             dlg += 1
 
     plt = pg.plot()
+    plt.addLegend()
 
     sp = pg.ScatterPlotWidget()
     fields = []
@@ -214,9 +228,9 @@ if __name__ == '__main__':
     sp.setFields(fields)
     sp.setData(examples)
 
-    ch = sp.colorMap.addNew('fit_success')
-    ch['Values', 'True'] = 'g'
-    ch['Values', 'False'] = 'r'
+    # ch = sp.colorMap.addNew('fit_success')
+    # ch['Values', 'True'] = 'g'
+    # ch['Values', 'False'] = 'r'
 
 
     sp.show()
@@ -225,13 +239,24 @@ if __name__ == '__main__':
     def pointsClicked(sp, pts):
         global sel
         sel = [pt.data() for pt in pts]
-        eval_fn = exp_fn
         plt.clear()
         for pt in pts:
             d = pt.data()
-            plt.plot(d['t'], d['y'])
-            plt.plot(d['t'], d['fit_y'], pen='g')
-            plt.plot(d['t'], d['true_y'], pen='b')
+            plt.plot(d['t'], d['y'], antialias=True, name='y')
+            plt.plot(d['t'], d['true_y'], pen={'color': 'w', 'style': pg.QtCore.Qt.DashLine}, antialias=True, name='true_y')
+            for i,method in enumerate(methods):
+                pfx = method.name + '_'
+                plt.plot(d['t'], d[pfx+'fit_y'], pen=(i, 5), antialias=True, name=method.name)
+
+            print("----------")
+            for n in d.dtype.names:
+                s = []
+                for i,line in enumerate(str(d[n]).split('\n')):
+                    if i > 0:
+                        line = ' '*30 + line
+                    s.append(line)
+                s = '\n'.join(s)
+                print("{:30s}{:s}".format(n, s))
 
         sp.setSelectedPoints(pts)
 
